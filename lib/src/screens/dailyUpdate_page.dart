1d37f80a436e65f8602044d64dda25f144ac2214
hilmihisham <hilmihisham@live.com>
2020-12-15 19:01:41 +0800

    show yesterday done task on Daily Update page

--- lib/src/screens/dailyUpdate_page.dart
+++ lib/src/screens/dailyUpdate_page.dart
@@ -1,13 +1,30 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:letsch_do/src/utils/database_helper.dart';
+import 'package:sqflite/sqflite.dart';
 
 // class for daily update screen, accessible via notification or sidebar menu
-class DailyUpdatePage extends StatelessWidget {
+class DailyUpdatePage extends StatefulWidget {
   
   final String payload;
 
   DailyUpdatePage({
     @required this.payload,
   });
+
+  @override
+  _DailyUpdatePageState createState() => _DailyUpdatePageState();
+}
+
+class _DailyUpdatePageState extends State<DailyUpdatePage> {
+
+  DatabaseHelper databaseHelper = DatabaseHelper();
+
+  // yesterday's message
+  int ytdDoneCount = 0;
+  String ytdDoneMsg = "";
+
 
   String getGreetingsByTime() {
     DateTime currentTime = DateTime.now();
@@ -24,13 +41,58 @@ class DailyUpdatePage extends StatelessWidget {
     }
   }
 
+  // 20201215 yesterday's task message [start]
+  void getYesterdayLogCount() {
+
+    debugPrint("getYesterdayLogCount() start");
+
+    final Future<Database> dbFuture = databaseHelper.initializeDatabase();
+    
+    List<String> randomMsg = [
+      "Keep up the good work!",
+      "Awesome!",
+      "Great job!",
+      "You rocks!",
+      "Noice!",
+      "Cool!",
+      "Brilliant!"
+    ];
+
+    dbFuture.then((database) {
+      var ytdCount = databaseHelper.getYesterdayDoneTaskCount();
+      ytdCount.then((value) => {
+        setState(() {
+          this.ytdDoneCount = value;
+
+          if (value > 0) {
+            // int randomizer = new Random().nextInt(7),
+            this.ytdDoneMsg = "You've finished " + value.toString() + " tasks yesterday. " + randomMsg.elementAt(Random().nextInt(7));
+          }
+          else {
+            this.ytdDoneMsg = "No finished task recorded yesterday. Well, people need break every now and then too.";
+          }
+        }),
+      });
+    });
+
+    debugPrint("getYesterdayLogCount() end");
+  }
+  // 20201215 yesterday's task message [end]
+
+  @override
+  void initState() {
+    getYesterdayLogCount();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Daily Update'),
       ),
-      body: Column(
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
@@ -42,8 +104,20 @@ class DailyUpdatePage extends StatelessWidget {
                 textScaleFactor: 1.3,
               ),
             ),
+
+            // 20201215 yesterday's task message [start]
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                this.ytdDoneMsg,
+                textAlign: TextAlign.center,
+                textScaleFactor: 1.3,
+              ),
+            ),
+            // 20201215 [end]
           ],
         ),
+      ),
     );
   }
 }
